a9ee74e27bfff5dae1e56c4a1ae403a64850a528
unknown <anderson.smachado@al.infnet.edu.br>
2022-10-29 16:02:05 -0300

    add menuOpRegistra()

--- TP1/TP1.java
+++ TP1/TP1.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class TP1 {
     private static final int TAMANHO = 2;
+    private static int      idx;
     private static String[] alunos;
     private static float[]  notaAv1;
     private static float[]  notaAv2;
 
     public static void varInicializa() {
+        idx     = -1;
         alunos  = new String[TAMANHO];
         notaAv1 = new float[TAMANHO];
         notaAv2 = new float[TAMANHO];
@@ -24,10 +26,35 @@ public class TP1 {
 			
 			System.out.print("Informe a opcao desejada: ");						
 			opcao = in.next();
+
+            switch (opcao) {
+                case "1":
+                    menuOpRegistra(in);
+                    break;
+                
+                default:
+                    break;
+            }
+
         } while (!"4".equalsIgnoreCase(opcao));   
 
         in.close();
     } 
+
+    public static void menuOpRegistra(Scanner in) {
+        if (idx < TAMANHO - 1) {
+            idx++;
+            System.out.print("Informe o seu nome: ");	
+			alunos[idx] = in.next();
+            System.out.print("Informe nota AV1: ");	
+			notaAv1[idx] = in.nextFloat();
+            System.out.print("Informe nota AV2: ");	
+			notaAv2[idx] = in.nextFloat();
+            System.out.println("Registro gravado na posição: "+idx);
+        } else {
+            System.out.println("Nao e possivel registrar novos alunos! Matriz cheia.");
+        }   
+    }
 
     public static void main(String[] args) {
         varInicializa();
