bb11b24019eea3ad40a04e4c61ef8fa22d81c1fa
unknown <anderson.smachado@al.infnet.edu.br>
2022-10-29 15:35:14 -0300

    Add menu principal

--- TP1/TP1.java
+++ TP1/TP1.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class TP1 {
+
+    public static void menu() {
+        Scanner in = new Scanner(System.in);
+        String opcao;
+
+        do {
+			System.out.println("[1] Registrar as notas de um novo aluno");
+			System.out.println("[2] Consultar boletim de um aluno");
+			System.out.println("[3] Consultar notas da turma");
+			System.out.println("[4] Sair");
+			
+			System.out.print("Informe a opcao desejada: ");						
+			opcao = in.next();
+        } while (!"4".equalsIgnoreCase(opcao));   
+
+        in.close();
+    } 
+
     public static void main(String[] args) {
-        System.out.println("Iniciando codificação");
+        menu();
+        System.out.println("Processamento finalizado!!!"); 
     }
 }
